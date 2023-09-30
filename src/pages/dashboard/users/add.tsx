9b6ce7e6ea9653170d8b835f979d2022f35cf1a0
bra40 <brian_rahadi@sfu.ca>
2023-09-30 11:01:06 -0700

    Implement initial UI for add form

--- src/pages/dashboard/users/add.tsx
+++ src/pages/dashboard/users/add.tsx
@@ -1,12 +1,22 @@
 import AdminNavbar from "@/components/AdminNavbar";
+import { LoginForm } from "@/components/LoginForm";
+import UserAddForm from "@/components/UserAddForm";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 export default function Page() {
   return (
-    <>
+    <div>
       <AdminNavbar />
-      <main>
-        <h1>Dashboard - Users add</h1>
+      <main className="flex min-h-screen items-center justify-center">
+        <Card className="w-full max-w-md">
+          <CardHeader>
+            <h1 className="text-2xl font-bold tracking-tight">User Add Form</h1>
+          </CardHeader>
+          <CardContent>
+            <UserAddForm />
+          </CardContent>
+        </Card>
       </main>
-    </>
+    </div>
   );
 }
