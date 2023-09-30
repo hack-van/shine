7d00c6e9c3731aa5d06e98fb62b932f78f82cdcf
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-30 00:44:35 -0700

    Add login form

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,63 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form"
+import { Input } from "./ui/input"
+import { Button } from "./ui/button"
+
+const loginSchema = z.object({
+  email: z.string().nonempty({
+    message: "Required"
+  }).email({
+    message: "Not a valid email"
+  }),
+  password: z.string().nonempty({
+    message: "Required"
+  })
+})
+
+type LoginSchema = z.infer<typeof loginSchema>
+
+export const LoginForm = () => {
+  const form = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+
+    }
+  })
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(console.log)} className="grid gap-2">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="name@example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
