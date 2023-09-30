bfab7bef25410c3f081a069d9ef0f7363d89298f
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-30 02:39:31 -0700

    Add index page

---
+++ src/pages/survey.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Page() {
+  return <>
+    <Head>
+      <title>Survey</title>
+    </Head>
+    <main className="min-h-screen flex justify-center p-4">
+      <div className="p-4 w-full max-w-md border rounded">
+        Hello
+      </div>
+    </main>
+  </>
+}
