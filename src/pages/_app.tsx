d20c9197fe44a6ac078a18c4e7879478ce2b00c2
Viet Hung Nguyen <vhn@sfu.ca>
2023-09-29 23:05:24 -0700

    Create Next app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,6 @@
-import { type AppType } from "next/app";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
-import { api } from "@/utils/api";
-
-import "@/styles/globals.css";
-
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
-
-export default api.withTRPC(MyApp);
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
