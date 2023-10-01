026cbeb55bd4ae9ebede72c65f3a92cfff6864a8
Ikenna Okpala <ikennajesse77@gmail.com>
2023-10-01 13:52:47 -0700

    cron sms

--- src/pages/sms.tsx
+++ src/pages/sms.tsx
@@ -1,21 +1,21 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { api } from "@/utils/api"
-import { useState } from "react"
+// import { Button } from "@/components/ui/button"
+// import { Input } from "@/components/ui/input"
+// import { api } from "@/utils/api"
+// import { useState } from "react"
 
-export default function Page() {
-  const [id, setId] = useState(4)
-  const [phone, setPhone] = useState("")
-  const {mutate} = api.sms.sendSms.useMutation()
+// export default function Page() {
+//   const [id, setId] = useState(4)
+//   const [phone, setPhone] = useState("")
+//   const {mutate} = api.sms.sendSms.useMutation()
 
-  return (
-    <>
-      <Input placeholder="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
-      <Input placeholder="form-id" value={id} onChange={(e) => setId(+e.target.value)} />
-      <Button onClick={() => mutate({
-        formId: id,
-        phone
-      })}>Send SMS</Button>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <Input placeholder="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+//       <Input placeholder="form-id" value={id} onChange={(e) => setId(+e.target.value)} />
+//       <Button onClick={() => mutate({
+//         formId: id,
+//         phone
+//       })}>Send SMS</Button>
+//     </>
+//   )
+// }
