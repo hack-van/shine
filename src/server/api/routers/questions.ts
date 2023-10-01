9138f86246f00b052154b605e6c6b73fb850c541
bra40 <brian_rahadi@sfu.ca>
2023-09-30 23:24:01 -0700

    Revert "Debug"
    This reverts commit b5297b550c7e3b9976482953402e8d4b40e1a16b.

--- src/server/api/routers/questions.ts
+++ src/server/api/routers/questions.ts
@@ -5,44 +5,9 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const questionRouter = createTRPCRouter({
   //get all questions
-  getAll: publicProcedure.query(async ({ ctx }) => {
-    // Get all questions
-    return ctx.db.select().from(questions);
-  }),
-
-  createQuestion: publicProcedure
-    .input(
-      z.object({
-        question: z.string(),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      return await ctx.db.insert(questions).values({
-        question: input.question,
-      });
+  getAll: publicProcedure
+    .query(async ({ ctx }) => {
+      // Get all questions
+      return ctx.db.select().from(questions)
     }),
-
-  updateQuestion: publicProcedure
-    .input(
-      z.object({
-        question: z.string(),
-        qid: z.number(),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      return await ctx.db
-        .update(questions)
-        .set({ question: input.question })
-        .where(eq(questions.qid, input.qid));
-    }),
-
-  deleteQuestion: publicProcedure
-    .input(
-      z.object({
-        qid: z.number(),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      return await ctx.db.delete(questions).where(eq(questions.qid, input.qid));
-    }),
-});
+})
