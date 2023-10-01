b5297b550c7e3b9976482953402e8d4b40e1a16b
Ikenna Okpala <ikennajesse77@gmail.com>
2023-09-30 21:04:04 -0700

    Debug

--- src/components/QuestionForm.tsx
+++ src/components/QuestionForm.tsx
@@ -1,27 +1,41 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form"
-import { Input } from "./ui/input"
-import { Button } from "./ui/button"
-
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { api } from "@/utils/api";
 
 const questionSchema = z.object({
   title: z.string().nonempty({
-    message: "Required"
-  })
-})
+    message: "Required",
+  }),
+});
 
-type QuestionSchema = z.infer<typeof questionSchema>
+type QuestionSchema = z.infer<typeof questionSchema>;
 
 export default function QuestionForm() {
   const form = useForm<QuestionSchema>({
-    resolver: zodResolver(questionSchema)
-  })
-
+    resolver: zodResolver(questionSchema),
+  });
+  const { mutate } = api.question.createQuestion.useMutation();
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(console.log)} className="grid gap-2">
+      <form
+        onSubmit={form.handleSubmit((data) =>
+          mutate({
+            question: data.title,
+          }),
+        )}
+        className="grid gap-2"
+      >
         <FormField
           control={form.control}
           name="title"
@@ -38,5 +52,5 @@ export default function QuestionForm() {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 }
---
+++ src/components/ui/QuestionSearch.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { Button } from "../ui/button";
+import { EditIcon, DeleteIcon } from "lucide-react";
+import { Divider } from "@nextui-org/react";
+
+const SearchResultWrapper = ({ question }) => {
+  return (
+    <div>
+      <div className="flex w-full justify-between p-4">
+        <span className="inline-bloack w-auto">{question}</span>
+        <div className="flex justify-between">
+          <EditIcon className="mr-4" color="white" size={18} />
+          <DeleteIcon className="mr-4" color="white" size={18} />
+        </div>
+      </div>
+      <Divider orientation="horizontal" />
+    </div>
+  );
+};
+
+export default function QuestionSearch({ questions }) {
+  console.log(questions);
+
+  const [searchInput, setSearchInput] = useState("");
+  const [filterQuestions, setFilterQuestions] = useState(questions);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
+  // useEffect(() => {
+  //   const filteredQuestions = questions.filter((question) => {
+  //     question.startsWith(searchInput);
+  //   });
+  //   setFilterQuestions(filteredQuestions);
+  // }, [searchInput]);
+
+  return (
+    <div className="mt-10">
+      <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+        <input
+          type="search"
+          className="relative m-0 -mr-0.5 block w-[1px] min-w-0 flex-auto rounded-bl rounded-br rounded-tl rounded-tr border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
+          aria-label="Search"
+          aria-describedby="button-addon3"
+          onChange={handleSearch}
+          value={searchInput}
+        />
+
+        <button
+          className="relative z-[2] ml-8 rounded border-2 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
+          type="button"
+          id="button-addon3"
+          data-te-ripple-init
+        >
+          Search
+        </button>
+      </div>
+
+      <div>
+        {questions.map((result, i) => (
+          <SearchResultWrapper key={i} question={result} />
+        ))}
+      </div>
+    </div>
+  );
+}
