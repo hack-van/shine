965522ba3e3f142ddafe1bd3f628229fbfd05d27
Viet Hung Nguyen <vhn@sfu.ca>
2023-10-01 10:10:35 -0700

    Add some users

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,10 @@
+import { api } from "@/utils/api"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { Loader2 } from "lucide-react"
 import { signIn } from "next-auth/react"
+import Head from "next/head"
 import { useRouter } from "next/router"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "./ui/button"
@@ -22,6 +26,7 @@ const loginSchema = z.object({
 type LoginSchema = z.infer<typeof loginSchema>
 
 export const LoginForm = () => {
+  const [isSubmitting, setSubmittng] = useState(false);
   const router = useRouter();
   const form = useForm<LoginSchema>({
     resolver: zodResolver(loginSchema),
@@ -30,52 +35,65 @@ export const LoginForm = () => {
       password: ""
     }
   })
+  // const { mutate } = api.user.addOne.useMutation()
 
   const onSubmit = async (data: LoginSchema) => {
-    const response = await signIn("credentials", {
-      ...data,
-      redirect: false
-    });
-    if (response?.ok) await router.replace("/dashboard")
-    if (response?.error) {
-      form.setError("root", { message: "Incorrect email or password" })
+    setSubmittng(true);
+    try {
+      const response = await signIn("credentials", {
+        ...data,
+        redirect: false
+      });
+      if (response?.ok) await router.replace("/dashboard")
+      if (response?.error) {
+        form.setError("root", { message: "Incorrect email or password" })
+      }
+    } finally {
+      setSubmittng(false)
     }
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-2">
-        {form.formState.errors.root?.message ?
-          <div className="text-sm text-destructive">{form.formState.errors.root.message}</div>
-          : null}
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="name@example.com" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Log in</Button>
-      </form>
-    </Form>
+    <>
+      <Head>
+        <title>Login | Shine - Youth Unlimited</title>
+      </Head>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-2">
+          {form.formState.errors.root?.message ?
+            <div className="text-sm text-destructive">{form.formState.errors.root.message}</div>
+            : null}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="name@example.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? <Loader2 className="animate-spin" /> : "Login"}
+          </Button>
+        </form>
+      </Form>
+    </>
   )
 }
--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,4 +1,6 @@
 import { users } from "@/server/db/schema";
+import { TRPCError } from "@trpc/server";
+import bcrypt from "bcrypt";
 import { eq } from "drizzle-orm";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
@@ -9,4 +11,25 @@ export const userRouter = createTRPCRouter({
       // Get all users
       return ctx.db.select().from(users)
     }),
+  addOne: publicProcedure
+    .input(z.object({
+      email: z.string().email(),
+      password: z.string(),
+    }))
+    .mutation(async ({ ctx, input }) => {
+      // Find a user
+      const result = await ctx.db.select().from(users).where(eq(users.email, input.email));
+      if (result.at(0)) {
+        throw new TRPCError({
+          message: "User already exists",
+          code: "CONFLICT",
+        })
+      }
+
+      const hashPassword = await bcrypt.hash(input.password, 10);
+      return ctx.db.insert(users).values({
+        email: input.email,
+        hashPassword,
+      })
+    })
 })
